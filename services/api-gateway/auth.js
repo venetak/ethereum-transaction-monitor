37af9c9a3d0794772f3c806fbb39b3a487fb3efb
venetak <veneta.kochovska93@gmail.com>
2023-08-27 02:20:33 +0300

    Document API Gateway

--- services/api-gateway/auth.js
+++ services/api-gateway/auth.js
@@ -1,5 +1,11 @@
 const secret = require('./config/secret');
 
+/**
+ * Authentication middleware. Sets a secret in the request headers.
+ * @param {Object} req - the request object
+ * @param {Object} res - the response object
+ * @param {function} next - the next middleware
+ */
 module.exports = (req, res, next) => {
     req.headers.secret = secret;
     next();
