83be19586ac6052fbc134522d173915964a624a0
venetak <veneta.kochovska93@gmail.com>
2023-08-27 22:42:55 +0300

    Update readme and add transactions table

---
+++ client/README.md
@@ -0,0 +1,17 @@
+# Client App
+
+This is is read-only representation of the Transactions database. It polls the server each 5 seconds to enable real-time update.
+
+## How to run
+
+Install all dependencies using
+
+```
+npm i
+```
+
+After that run
+
+```
+npm run start
+```
--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,103 @@
 import React, { useEffect, useState } from 'react';
-import { Space, Table, Tag } from 'antd';
+import { Table, Tag, Form, Layout } from 'antd';
 
 const POLLING_INTERVAL = 5000;
 
-  const columns = [
-    {
-      title: 'hash',
-      dataIndex: 'hash',
-    },
-    {
-      title: 'to',
-      dataIndex: 'to',
-    },
-    {
-      title: 'from',
-      dataIndex: 'from',
-    },
-    {
-      title: 'nonce',
-      dataIndex: 'nonce',
-    },
-    {
-      title: 'Gas Limit',
-      dataIndex: 'gasLimit',
-    },
-    {
-      title: 'Gas Price',
-      dataIndex: 'gasPrice',
-    },
-    {
-      title: 'Max Fee Per Gas',
-      dataIndex: 'maxFeePerGas',
-    },
-    {
-      title: 'Chain Id',
-      dataIndex: 'chainId',
-    },
-    {
-      title: 'Config Id',
-      dataIndex: 'configId',
-    },
-  ];
+const columns = [
+  {
+    title: 'hash',
+    dataIndex: 'hash',
+  },
+  {
+    title: 'to',
+    dataIndex: 'to',
+  },
+  {
+    title: 'from',
+    dataIndex: 'from',
+  },
+  {
+    title: 'nonce',
+    dataIndex: 'nonce',
+  },
+  {
+    title: 'Gas Limit',
+    dataIndex: 'gasLimit',
+  },
+  {
+    title: 'Gas Price',
+    dataIndex: 'gasPrice',
+  },
+  {
+    title: 'Max Fee Per Gas',
+    dataIndex: 'maxFeePerGas',
+  },
+  {
+    title: 'Chain Id',
+    dataIndex: 'chainId',
+  },
+  {
+    title: 'Config Id',
+    dataIndex: 'configId',
+  },
+];
 
-  const App = () => {
-    const [data, setData] = useState();
-    const [loading, setLoading] = useState(false);
-    const [tableParams, setTableParams] = useState({
-      pagination: {
-        current: 1,
-        pageSize: 10,
-      },
-    });
-    const fetchData = () => {
-      setLoading(true);
-      fetch(`http://localhost:9444/transactions`)
-        .then((res) => res.json())
-        .then(({ data }) => {
-          setData(data);
-          setLoading(false);
-          setTableParams({
-            ...tableParams,
-            pagination: {
-              ...tableParams.pagination,
-              total: data.length,
-              // 200 is mock data, you should read it from server
-              // total: data.totalCount,
-            },
-          });
+const LayoutStyle = {
+  padding: 40
+};
+
+const App = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [tableParams, setTableParams] = useState({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+  });
+  
+  const fetchData = () => {
+    setLoading(true);
+    fetch(`http://localhost:9444/transactions`)
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setData(data);
+        setLoading(false);
+        setTableParams({
+          ...tableParams,
+          pagination: {
+            ...tableParams.pagination,
+            total: data.length,
+          },
         });
-    };
-  
-    useEffect(() => {
-      if (window.polling) clearInterval(window.polling);
-      window.polling = setInterval(fetchData, POLLING_INTERVAL)
-      // fetchData();
-    }, [JSON.stringify(tableParams)]);
-    const handleTableChange = (pagination, filters, sorter) => {
-      setTableParams({
-        pagination,
-        filters,
-        ...sorter,
       });
-  
-      // `dataSource` is useless since `pageSize` changed
-      if (pagination.pageSize !== tableParams.pagination?.pageSize) {
-        setData([]);
-      }
-    };
-    return (
+  };
+
+  useEffect(() => {
+    fetchData()
+
+    if (window.polling) clearInterval(window.polling);
+    window.polling = setInterval(() => {
+      fetchData();
+    }, POLLING_INTERVAL)
+  }, [JSON.stringify(tableParams)]);
+
+  const handleTableChange = (pagination, filters, sorter) => {
+    setTableParams({
+      pagination,
+      filters,
+      ...sorter,
+    });
+
+    // `dataSource` is useless since `pageSize` changed
+    if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+      setData([]);
+    }
+  };
+
+  return (
+    <Layout style={LayoutStyle}>
+      <h1>Transactions</h1>
       <Table
         columns={columns}
         rowKey={(record) => record._id}
@@ -96,6 +106,8 @@ const POLLING_INTERVAL = 5000;
         loading={loading}
         onChange={handleTableChange}
       />
-    );
-  };
-  export default App;
+    </Layout>
+
+  );
+};
+export default App;
